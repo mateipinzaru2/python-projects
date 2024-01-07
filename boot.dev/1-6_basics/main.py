6e4420baffca2d7c3a6ffb286f1c2cd8fa4a2ff0
Matei Pinzaru <matei@avocado.solutions>
2024-01-07 18:35:57 +0200

    initial commit

---
+++ boot.dev/1-6_basics/main.py
@@ -0,0 +1,31 @@
+# exponent
+print(2 ** 3) # 8
+
+# floor
+print(250 // 150) # 1
+
+# don't do this
+print(250 / 0) # throws error
+
+"""
+bitwise & (AND) operator
+0101 = 5
+&
+0111 = 7
+=
+0101 = 5
+
+bitwise | (OR) operator
+0101 = 5
+|
+0111 = 7
+=
+0111 = 7
+
+NOT operator
+print(not True)
+# Prints: False
+
+print(not False)
+# Prints: True
+"""
