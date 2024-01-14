6cf72ec733c312f48ab65f3a38dcdef8020699e0
Matei Pinzaru <matei@avocado.solutions>
2024-01-14 11:03:35 +0200

    improved azure_get_user_group_membership_and_last_signin to also fetch MFA Registration status for each user ⚡

--- azure_get_user_group_membership_and_last_signin/graphsdk.py
+++ azure_get_user_group_membership_and_last_signin/graphsdk.py
@@ -5,6 +5,9 @@ import csv
 from azure.identity import ClientSecretCredential
 from msgraph import GraphServiceClient
 from msgraph.generated.users.users_request_builder import UsersRequestBuilder
+from msgraph.generated.reports.authentication_methods.user_registration_details.user_registration_details_request_builder import (
+    UserRegistrationDetailsRequestBuilder,
+)
 from kiota_abstractions.api_error import APIError
 
 
@@ -29,26 +32,34 @@ client = GraphServiceClient(
 )
 
 
-query_params = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(
+user_query_params = UsersRequestBuilder.UsersRequestBuilderGetQueryParameters(
     filter="accountEnabled eq true",
-    select=["userPrincipalName", "signInActivity"],
     expand=["memberOf"],
     top=999,
 )
-request_config = UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
-    query_parameters=query_params
+user_request_config = UsersRequestBuilder.UsersRequestBuilderGetRequestConfiguration(
+    query_parameters=user_query_params
+)
+
+
+mfa_query_params = UserRegistrationDetailsRequestBuilder.UserRegistrationDetailsRequestBuilderGetQueryParameters(
+    filter="isMfaRegistered eq true",
+    top=999,
+)
+mfa_request_config = UserRegistrationDetailsRequestBuilder.UserRegistrationDetailsRequestBuilderGetRequestConfiguration(
+    query_parameters=mfa_query_params
 )
 
 
 async def get_user_pages():
     try:
         user_pages = []
-        page = await client.users.get(request_configuration=request_config)
+        page = await client.users.get(request_configuration=user_request_config)
         user_pages.append(page)
 
         while page.odata_next_link:
             page = await client.users.with_url(page.odata_next_link).get(
-                request_configuration=request_config
+                request_configuration=user_request_config
             )
             user_pages.append(page)
 
@@ -58,12 +69,43 @@ async def get_user_pages():
     return user_pages
 
 
+async def get_mfa_pages():
+    try:
+        mfa_pages = []
+        page = (
+            await client.reports.authentication_methods.user_registration_details.get(
+                request_configuration=mfa_request_config
+            )
+        )
+        mfa_pages.append(page)
+
+        while page.odata_next_link:
+            page = await client.reports.authentication_methods.user_registration_details.with_url(
+                page.odata_next_link
+            ).get(
+                request_configuration=mfa_request_config
+            )
+            mfa_pages.append(page)
+
+    except APIError as e:
+        print(f"Error: {e.error.message}")
+
+    return mfa_pages
+
+
 async def process_users():
     users_pages = await get_user_pages()
+    mfa_pages = await get_mfa_pages()
+    mfa_dict = {}
+    if mfa_pages:
+        for mfas in mfa_pages:
+            if mfas and mfas.value:
+                for mfa in mfas.value:
+                    mfa_dict[mfa.id] = "Yes" if mfa.is_mfa_registered else "No"
     if users_pages:
         with open("graphsdk_output.csv", "w", newline="") as file:
             writer = csv.writer(file)
-            writer.writerow(["User", "Sign in date", "Groups"])
+            writer.writerow(["User", "MFA Registered", "Sign in date", "Groups"])
             for users in users_pages:
                 if users and users.value:
                     for user in users.value:
@@ -79,8 +121,14 @@ async def process_users():
                             if user.sign_in_activity
                             else "N/A"
                         )
+                        mfa_registered = mfa_dict.get(user.id, "No")
                         writer.writerow(
-                            [user.user_principal_name, sign_in_date, groups]
+                            [
+                                user.user_principal_name,
+                                mfa_registered,
+                                sign_in_date,
+                                groups,
+                            ]
                         )
 
 
--- azure_get_user_group_membership_and_last_signin/readme.md
+++ azure_get_user_group_membership_and_last_signin/readme.md
@@ -1,13 +1,17 @@
 # Azure Graph API Client
-This is a Python client for the `Microsoft Graph API`. It fetches all users from the API and for each user, it fetches the last sign-in activity and the group membership. It then stores the data in a CSV file. There are 3 versions of the program:
+This is a Python client for the `Microsoft Graph API`. It fetches all users from the API and for each user, it fetches the last sign-in activity and group membership. It then stores the data in a CSV file. 
+
+*The `graphsdk.py` variant also fetches the MFA registration status for each user.* 
+
+There are 3 versions of the program:
 
 > [!CAUTION]
-> Very slow variants :turtle:. DO NOT USE:
+> DEPRECATED. Very slow variants :turtle:. DO NOT USE:
 - `singlethreaded.py` - single-threaded version
 - `multithreaded.py` - multithreaded version
 
 > [!TIP]
-> Fast variant :rocket:. USE THIS:
+> LTS. Fast variant :rocket:. USE THIS:
 - `graphsdk.py` - async version using the Python Microsoft Graph SDK
 
 ## Pre-requisites
@@ -36,13 +40,13 @@ The main loop of the program fetches users in batches of ***999*** from the API 
 Due to the API rate limit, there isn't much of a difference between the single-threaded and multithreaded versions of the program. The multithreaded version is only slightly faster because it can make more requests in parallel, but it still has to wait for the API to respond.
 
 ### Single Threaded Stats
-For ***3005*** users **->** ***6h 11m 46s*** execution time
+For ***3005*** users **->** ***6h 11m 46s*** execution time.
 
 ### Multi-threaded Stats
-For ***3005*** users **->** ***N/A*** execution time
+For ***3005*** users **->** ***N/A*** execution time.
 
 ## `graphsdk.py` Performance
 Instead of making `2n` API calls for each user to get the group membership and last sign in date, the program's performance is ***massively improved*** by using the [Python Microsoft Graph SDK](https://github.com/microsoftgraph/msgraph-sdk-python) to fetch batches of ***999*** users in a single API call, and expanding the response to include the group membership and last sign-in date, in one go. This is possible via [query parameters](https://learn.microsoft.com/en-us/graph/query-parameters?tabs=http#expand-parameter). The time complexity of this variant is `O(n/999)`, which simplifies to `O(1)` *if `n` is small*.
 
 ### Graph SDK Stats
-For ***8749*** users **->** ***1m 55s*** execution time
+For ***3015*** users **->** ***58s*** execution time.
